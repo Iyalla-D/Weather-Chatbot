b44f780ca01608d625ee3b09a8903bcf8d43da23
Yaroslav Makhynia <yaroslav.makhynia@student.griffith.ie>
2021-04-02 12:47:46 +0100

    TestBot class added with main method

---
+++ TestBot.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TestBot {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String userInput = "";
+        MyBot chatbot = new MyBot();
+ 
+        while(!userInput.equalsIgnoreCase("stop it")) {
+            System.out.print("User: ");
+            userInput = sc.nextLine();
+            chatbot.answer(userInput);
+        }
+        sc.close();
+    }
+	}
+
+
