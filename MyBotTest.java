6221debf180ac355d99cdee6a36dee10aa22042c
Tamunoiyalla Dinney <tamunoiyallawilliam.dinney@student.griffith.ie>
2021-04-02 12:21:48 +0100

    MyBotTest.java

---
+++ MyBotTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+class MyBotTest {
+
+	@Test
+	void testMyBot01() {
+		MyBot Bot = new MyBot();
+		
+		String input="Hi";
+		String actual="";
+		Set<String> keys = Bot.getKnowledge().keySet();
+		 for (String key : keys){
+			 String lowerKey = key.toLowerCase();
+	         String lowerQuestion = input.toLowerCase();
+	         if (lowerKey.contains(lowerQuestion)) {
+	               actual = Bot.getKnowledge().get(key);
+	            }
+		 }
+		String expected = "Hello... Please to meet you!";
+		 
+		assertEquals(expected,actual);
+	}
+
+}
