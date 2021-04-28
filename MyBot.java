68d8d07c79ddbe3d47e120f14352a99b5e301307
Yaroslav <yaroslav.makhynia@student.griffith.ie>
2021-04-28 21:58:09 +0000

    MyBot.java updated. Everything from JsonTest is now done in this file, also test. Java swing added for GUI (needs to be designed and scroll bar needs to be added)

--- MyBot.java
+++ MyBot.java
@@ -1,10 +1,21 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.Scanner;
 import java.util.Set;
 
-public class MyBot {
-	 HashMap<String, String> knowledge = new HashMap<String, String>();
+import javax.swing.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+public class MyBot extends JFrame{
+	HashMap<String, String> knowledge = new HashMap<String, String>();
+	JTextArea chatArea = new JTextArea();
+	JTextField chatbox= new JTextField();
+		public static void main(String[] args) {
+			new MyBot();
+	    }
     /**
      * This is a default constructor.
      */
@@ -16,6 +27,33 @@ public class MyBot {
        knowledge.put("Hello","Hi there, What can i help you with? ");
        knowledge.put("What do i do","Im here to help you pick out an out based on the weather.");
        knowledge.put("Do i just tell you the name of the place","Yes please ");
+       JFrame frame = new JFrame("Chatbot");
+       frame.setSize(600, 600);
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setResizable(false);
+       frame.setLayout(null);
+       frame.add(chatbox);
+       chatArea.setSize(500, 400);
+       chatArea.setLocation(2, 2);
+       chatArea.setLineWrap(true);
+       chatArea.setWrapStyleWord(true);
+       chatArea.setBounds(50, 50, 500, 300);
+       chatArea.setEditable(false);
+       chatbox.setSize(540, 30);
+       chatbox.setLocation(2, 500);
+       frame.add(chatArea);
+       frame.setVisible(true);
+       chatbox.addActionListener(new ActionListener() {
+
+    	   @Override
+    	   public void actionPerformed(ActionEvent arg0) {
+    		   String gtext = chatbox.getText();
+    		   chatArea.append("User: " + gtext+"\n");
+    		   chatbox.setText("");
+    		   answer(gtext);
+			}
+			
+		});
     }
     
     public void answer(String question) {
@@ -24,24 +62,16 @@ public class MyBot {
             String lowerKey = key.toLowerCase();
             String lowerQuestion = question.toLowerCase();
             if (lowerKey.contains(lowerQuestion)) {
-               System.out.println("Bot: " + knowledge.get(key));
-               System.out.println("Bot: " + "If you want the weather, enter a location");
+            	
+               chatArea.append("Bot: " + knowledge.get(key)+"\n");
+               chatArea.append("Bot: " + "If you want the weather, enter a location \n");
                return;//break
             }
               
         }
         weather(question);
-        //trainMe(question);
     }
-    
-    public void trainMe(String question) {
-        System.out.println("Bot: Sorry, Im dumb! How should I reply");
-        System.out.print("User suggestion: ");
-        Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-        knowledge.put(question, userInput);
-    }
-    
+        
     public HashMap<String,String> getKnowledge(){
     	return knowledge;
     }
@@ -54,15 +84,69 @@ public class MyBot {
 	           //Connect to a HTTP url with "Connect Method", "User" and "Password" if required only.
 	           String json = ht.httpConnect("", "GET", "user", "password");
 	           json = json.substring(0, json.length()-1);
-	           //System.out.println(json);
-	           JsonTest parse= new JsonTest();
-	           parse.getdata(json);
+	           getdata(json);
 	           } 
 	       	catch (Exception e) {
-	       		System.out.println("Not a location, Try again");
+	       		chatArea.append("Not a location, Try again \n");
 	           //e.printStackTrace(); 
 	           
 	       }
 
     }
+    private boolean getdata(String json) throws Exception {
+
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            Object obj = parser.parse(json);
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray tempData = (JSONArray) jsonObject.get("weather");
+
+            chatArea.append("Weather: " + ((JSONObject) tempData.get(0)).get("main")+"\n");
+            if(((JSONObject) tempData.get(0)).get("main").equals("Clouds")) {
+            	 chatArea.append("Wear a sweater and maybe bring an umbrella"+"\n");
+            }
+           
+            chatArea.append("Weather Description: " + ((JSONObject) tempData.get(0)).get("description")+"\n");
+
+            JSONObject tempData1 = (JSONObject) jsonObject.get("main");
+
+            chatArea.append("Temperature: " +  (Double.parseDouble(tempData1.get("temp").toString()) - 273.15)+" Deg C\n"); //in Deg C
+            chatArea.append("Temperature Min: "+(Double.parseDouble(tempData1.get("temp_min").toString()) - 273.15)+" Deg C\n");//in Deg C
+            chatArea.append("Temperature Max: "+ (Double.parseDouble(tempData1.get("temp_max").toString()) - 273.15)+" Deg C\n");//in Deg C
+            chatArea.append("Pressure: " + tempData1.get("pressure")+"\n"); //hpa
+            chatArea.append("Humidity: " + tempData1.get("humidity")+"\n"); //%
+            if((Double.parseDouble(tempData1.get("temp").toString()) - 273.15)<-10.00) {
+         	   System.out.println();
+            }
+            else if((Double.parseDouble(tempData1.get("temp").toString()) - 273.15)<0.00) {
+         	   System.out.println();
+            }
+            else if((Double.parseDouble(tempData1.get("temp").toString()) - 273.15)<4.00) {
+         	   System.out.println();
+            }
+            else if((Double.parseDouble(tempData1.get("temp").toString()) - 273.15)<15.00) {
+            	chatArea.append("The weather cold so bring a jacket");
+            }
+           else if((Double.parseDouble(tempData1.get("temp").toString()) - 273.15)<20.00) {
+        	   System.out.println();
+           }
+           else if((Double.parseDouble(tempData1.get("temp").toString()) - 273.15)<25.00) {
+        	   System.out.println();
+           }
+           else if((Double.parseDouble(tempData1.get("temp").toString()) - 273.15)<30.00) {
+        	   System.out.println();
+           }
+           else if((Double.parseDouble(tempData1.get("temp").toString()) - 273.15)<37.00) {
+        	   System.out.println();
+           }
+
+        } catch (Exception e) {
+            //e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
 }
